5467de65287ca2bac3b1a74fca6f1d39817fc40c
theteachr <sop.theteachr@gmail.com>
2022-10-03 01:19:34 +0530

    Partially working

--- src/Main.elm
+++ src/Main.elm
@@ -1,62 +1,109 @@
 module Main exposing (..)
 
 import Browser
-
-import Html exposing (text, div, button)
+import Html exposing (button, div, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+
+
 
 -- main
 -- update
 -- view
+
 
 main =
     Browser.sandbox { init = init, update = update, view = view }
 
 
 type alias Model =
-    { onScreen : String
-    , current : String
-    , value : Float
+    { currentValue : Float
+    , currentValueString : String
     , currentOp : Float -> Float
     }
 
+
+type Op
+    = Add
+    | Sub
+    | Mul
+    | Div
+
+
+toFn op =
+    case op of
+        Add ->
+            (+)
+
+        Sub ->
+            (-)
+
+        Mul ->
+            (*)
+
+        Div ->
+            (/)
+
+
 init : Model
-init = Model "0" "" 0.0 identity
+init =
+    Model 0.0 "" identity
+
 
 type Msg
     = Number Int
-    | Perform (Float -> Float -> Float)
+    | Perform Op
     | Reset
+    | Eval
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Number n ->
             let
-                newValue = model.current ++ (String.fromInt n)
+                newValue =
+                    model.currentValueString ++ String.fromInt n
             in
             { model
-            | onScreen = newValue
-            , current = newValue
+                | currentValueString = newValue
+                , currentValue = newValue |> String.toFloat |> Maybe.withDefault 0
             }
 
         Perform op ->
-            { model | currentOp = op (String.toFloat model.current
-                |> Maybe.withDefault 0) }
+            let
+                updatedValue = model.currentOp model.currentValue
+            in
+            { model
+                | currentValue = updatedValue
+                , currentOp = updatedValue |> toFn op
+                , currentValueString = ""
+            }
+
+        Eval ->
+            let
+                result =
+                    model.currentOp model.currentValue
+            in
+            { model
+                | currentValue = result
+                , currentValueString = ""
+                , currentOp = identity
+            }
 
         Reset ->
             init
 
+
 view model =
     div [ class "base" ]
-        [ div [ class "display" ] [ text model.onScreen ]
+        [ div [ class "display" ] [ text (String.fromFloat model.currentValue) ]
         , div [ class "buttons" ]
-            [ button [ class "operator", onClick (Perform (+))] [ text "+" ]
-            , button [ class "operator", onClick (Perform (-))] [ text "−" ]
-            , button [ class "operator", onClick (Perform (*))] [ text "×" ]
-            , button [ class "operator", onClick (Perform (/))] [ text "÷" ]
-            , button [ class "equals" ] [ text "=" ]
+            [ button [ class "operator", onClick (Perform Add) ] [ text "+" ]
+            , button [ class "operator", onClick (Perform Sub) ] [ text "−" ]
+            , button [ class "operator", onClick (Perform Mul) ] [ text "×" ]
+            , button [ class "operator", onClick (Perform Div) ] [ text "÷" ]
+            , button [ class "equals", onClick Eval ] [ text "=" ]
             , button [ onClick (Number 7) ] [ text "7" ]
             , button [ onClick (Number 8) ] [ text "8" ]
             , button [ onClick (Number 9) ] [ text "9" ]
@@ -71,3 +118,15 @@ view model =
             , button [ onClick Reset ] [ text "C" ]
             ]
         ]
+
+
+
+-- { cV = 0, cO = i, cS = "" }
+----> Number 1
+-- { cV = 1, cO = i, cS = "1" }
+----> Perform Add
+-- { cV = 1, cO = (+) 1, cS = "" }
+----> Number 2
+-- { cV = 2, cO = (+) 86, cS = "1"
+----> Eval
+-- { cV = 87, cO = i, cS = "" }
